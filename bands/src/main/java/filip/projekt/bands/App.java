d54622386f18ed9adebdb03ec693f45d44feca4d
dajkam <sopotfilip@onet.pl>
2018-02-26 13:13:48 +0100

    dodany maven

---
+++ bands/src/main/java/filip/projekt/bands/App.java
@@ -0,0 +1,13 @@
+package filip.projekt.bands;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
