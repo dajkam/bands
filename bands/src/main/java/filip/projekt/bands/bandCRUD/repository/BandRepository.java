9986dcc4fc86d846c325c59d5f50ac7af6b47c5b
dajkam <sopotfilip@onet.pl>
2018-03-19 14:03:38 +0100

    poprawione testy repozytorium

--- bands/src/main/java/filip/projekt/bands/bandCRUD/repository/BandRepository.java
+++ bands/src/main/java/filip/projekt/bands/bandCRUD/repository/BandRepository.java
@@ -3,13 +3,16 @@ package filip.projekt.bands.bandCRUD.repository;
 import filip.projekt.bands.bandCRUD.domain.Band;
 import java.util.*;
 public interface BandRepository {
+  public void initDB();
+  public List<Band> getAll();
+  public void add(Band b);
+  public Band getBy_Id(int _Id);
   public String getInfo();
   public void createTableBand();
-  public List<Band> getAll();
-  public void selectFromBand(int _Id);
+  public void dropTableBand();
+  public Band selectFromBand(int _Id);
   public void deleteFromBand(int _Id);
   public void insertToBand(Band b);
   public void updateById(int _Id,Band b);
-    public void dropTableBand();
 
 }
