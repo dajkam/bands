4bdb2703955ba48a980cf5bc72efeecf1be0652a
dajkam <sopotfilip@onet.pl>
2018-03-25 01:02:49 +0100

    wstępny CRUD

--- bands/src/main/java/filip/projekt/bands/bandCRUD/repository/BandRepository.java
+++ bands/src/main/java/filip/projekt/bands/bandCRUD/repository/BandRepository.java
@@ -1,18 +1,22 @@
 package filip.projekt.bands.bandCRUD.repository;
 
 import filip.projekt.bands.bandCRUD.domain.Band;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.*;
 public interface BandRepository {
-  public void initDB();
+  public Connection getConnection();
+  public void setConnection(Connection con ) throws SQLException;
   public List<Band> getAll();
-  public void add(Band b);
-  public Band getBy_Id(int _Id);
+  public int add(Band b);
+  public Band getBy_Id(int _Id) throws SQLException;
   public String getInfo();
-  public void createTableBand();
-  public void dropTableBand();
-  public Band selectFromBand(int _Id);
-  public void deleteFromBand(int _Id);
-  public void insertToBand(Band b);
-  public void updateById(int _Id,Band b);
+  public void createTableBand() throws SQLException;
+  public void dropTableBand() throws SQLException;
+  public Band selectFromBand(int _Id) throws SQLException;
+  public int deleteFromBand(Band b);
+  public int updateBand(Band b) throws SQLException;
+  public int deleteAll();
 
 }
