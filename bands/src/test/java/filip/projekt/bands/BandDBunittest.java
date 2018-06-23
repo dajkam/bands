0ed3ac4f7a9fdaf6e86044e59491d7a8601ff6b2
dajkam <sopotfilip@onet.pl>
2018-06-23 19:16:30 +0200

    działający dbunit

--- bands/src/test/java/filip/projekt/bands/BandDBunittest.java
+++ bands/src/test/java/filip/projekt/bands/BandDBunittest.java
@@ -13,6 +13,7 @@ import java.util.LinkedList;
 import java.util.Random;
 
 import org.hamcrest.core.IsEqual;
+import org.hsqldb.persist.PersistentStoreCollection;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -28,6 +29,8 @@ import java.sql.DriverManager;
 
 import org.dbunit.Assertion;
 import org.dbunit.DBTestCase;
+import org.dbunit.JdbcDatabaseTester;
+import org.dbunit.PropertiesBasedJdbcDatabaseTester;
 import org.dbunit.dataset.IDataSet;
 import org.dbunit.dataset.ITable;
 import org.dbunit.dataset.filter.DefaultColumnFilter;
@@ -51,19 +54,29 @@ public class BandDBunittest extends DBTestCase {
 
     @After
     public void tearDown() throws Exception {
-        //super.tearDown();
+      //  super.tearDown();
+      //repo.deleteAll();
        
     }
 
     @Before
     public void setUp() throws Exception {
-       // super.setUp();
+      // super.setUp();
         repo = new BandRepositoryFactory(DriverManager.getConnection(url));
+
+       FlatXmlDataSet dataSet = new FlatXmlDataSetBuilder().build(
+            BTest.class.getClassLoader().
+                        getResource("ds-0.xml").openStream()
+        );
+        JdbcDatabaseTester databaseTester = new PropertiesBasedJdbcDatabaseTester();
+        databaseTester.setSetUpOperation(DatabaseOperation.CLEAN_INSERT);
+        databaseTester.setDataSet(dataSet);
+        databaseTester.onSetup();
     }
 
     @Test
     public void doNothing() {
-       assertEquals(3, repo.getAll().size());
+      // assertEquals(3, repo.getAll().size());
     }
 
     @Test
@@ -91,8 +104,117 @@ public class BandDBunittest extends DBTestCase {
         // (posortowane? proszę bardzo:) // Assertion.assertEquals(new SortedTable(expectedTable),
         // (posortowane? proszę bardzo:) //     new SortedTable(filteredTable, expectedTable.getTableMetaData()));
       // repo.deleteFromBand(band);
-      assertEquals(4, repo.getAll().size());
+     // assertEquals(4, repo.getAll().size());
         Assertion.assertEquals(expectedTable , filteredTable);
+        repo.deleteFromBand(band);
+                                     // wyczyszczenie
+    }
+    
+    @Test
+    public void checkDeleteByName() throws Exception {
+   
+        Band band = new Band();
+
+        band.setName("Def Leppard");
+        band.setGenre("Glam Metal");
+        band.setNumberOfMembers(5);
+
+        if (!repo.searchBand(band.getName()).isEmpty()) {
+            repo.deleteFromBandByName(band);
+            
+        }
+
+      
+       repo.deleteFromBandByName(band);
+
+       Band band2 = new Band();
+            band2.setName("Pantera");
+            band2.setGenre("Groove Metal");
+            band2.setNumberOfMembers(4);
+            if (!repo.searchBand(band2.getName()).isEmpty()) {
+                repo.deleteFromBandByName(band2);
+                
+            }
+            
+        
+
+      
+
+       
+
+        IDataSet dbDataSet = this.getConnection().createDataSet();
+        ITable actualTable = dbDataSet.getTable("Band");
+        ITable filteredTable = DefaultColumnFilter.excludedColumnsTable(actualTable, new String[] { "ID" });
+        IDataSet expectedDataSet = getDataSet("ds-3.xml");
+        ITable expectedTable = expectedDataSet.getTable("Band");
+        
+       
+      
+        Assertion.assertEquals(expectedTable , filteredTable);
+                                     // wyczyszczenie
+    }
+
+    @Test
+    public void checkUpdating() throws Exception {
+        
+
+
+        Band band = new Band();
+        
+        band.setName("Def Leppard");
+        band.setGenre("Glam Metal");
+        band.setNumberOfMembers(5);
+
+        Band band2 = new Band();
+          
+        band2.setName("Pantera");
+        band2.setGenre("Groove Metal");
+        band2.setNumberOfMembers(4);
+
+     
+        
+        
+
+       if (!repo.searchBand(band.getName()).isEmpty()) {
+             repo.deleteFromBandByName(band);
+           
+       }
+
+       if (!repo.searchBand(band2.getName()).isEmpty()) {
+            repo.deleteFromBandByName(band2);
+      
+  }
+
+        Band band3 = new Band();
+                
+        band3.setName("Descotica");
+        band3.setGenre("Disco Polo");
+        band3.setNumberOfMembers(4);
+        band3.setId(repo.searchBand("Metallica").get(0).getId());
+
+        Band band4 = new Band();
+                
+        band4.setName("Discohead");
+        band4.setGenre("Rock Polo");
+        band4.setNumberOfMembers(3);
+        band4.setId(repo.searchBand("Motorhead").get(0).getId());
+
+        repo.updateBand(band3);
+
+        repo.updateBand(band4);
+        
+
+       
+
+        IDataSet dbDataSet = this.getConnection().createDataSet();
+        ITable actualTable = dbDataSet.getTable("Band");
+        ITable filteredTable = DefaultColumnFilter.excludedColumnsTable(actualTable, new String[] { "ID" });
+        IDataSet expectedDataSet = getDataSet("ds-4.xml");
+        ITable expectedTable = expectedDataSet.getTable("Band");
+       
+   //   assertEquals(4, repo.getAll().size());
+        Assertion.assertEquals(expectedTable , filteredTable);
+       // repo.deleteFromBand(band);
                                      // wyczyszczenie
     }
 
